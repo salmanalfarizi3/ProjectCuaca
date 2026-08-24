ef04a3a7ffa19a71ba13bb606f28ed5411c96575
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-08-24 19:54:50 +0700

    feat(model): buat model User

---
+++ models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      User.hasMany(models.ApiKey, { foreignKey: 'userId', as: 'apiKeys' });
+    }
+  }
+  User.init({
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false }
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users'
+  });
+  return User;
+};

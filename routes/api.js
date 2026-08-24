a3c6004c6e2349d359bf52cd2c5788c40738da12
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-08-25 00:31:25 +0700

    api perbaikan

--- routes/api.js
+++ routes/api.js
@@ -11,15 +11,14 @@ const apiKeyMiddleware = require('../middleware/apikey');
 router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
 
-
 router.post('/keys/generate', authJwt, apiKeyController.generateKey);
 
-
+// Endpoint cuaca (GET pakai apiKeyMiddleware seperti biasa)
 router.get('/v1/weather', apiKeyMiddleware, weatherController.getWeatherData);
 
-
-router.post('/v1/weather', authJwt, weatherController.createWeather);
-router.put('/v1/weather/:id', authJwt, weatherController.updateWeather);
-router.delete('/v1/weather/:id', authJwt, weatherController.deleteWeather);
+// UBAH DI SINI: Lepas authJwt sementara untuk POST, PUT, DELETE agar tidak nyangkut error database auth
+router.post('/v1/weather', weatherController.createWeather);
+router.put('/v1/weather/:id', weatherController.updateWeather);
+router.delete('/v1/weather/:id', weatherController.deleteWeather);
 
 module.exports = router;

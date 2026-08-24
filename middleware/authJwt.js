5a72a3df7d88f3c0d412bd6c70828eb7641e5900
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-08-24 19:53:19 +0700

    feat(middleware): buat middleware JWT auth

---
+++ middleware/authJwt.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ message: 'Authorization header missing' });
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.JWT_SECRET || 'mysupersecretkey123', (err, user) => {
+    if (err) return res.status(403).json({ message: 'Invalid or expired JWT' });
+    req.user = user;
+    next();
+  });
+};

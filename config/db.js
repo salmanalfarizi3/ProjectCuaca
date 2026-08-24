980f0137918ac38f9bd50843fabf826141d991df
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-08-24 23:23:03 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,19 +1,33 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const env = process.env.NODE_ENV || 'development';
-const config = require('./config')[env];
-
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  const env = process.env.NODE_ENV || 'development';
+  const config = require('./config')[env];
+  
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  } else {
+    sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config
+    );
+  }
 }
 
 module.exports = sequelize;

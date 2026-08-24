b4afb23908dc4bbbad525833e4e2f90e6eff4b53
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-08-24 19:55:17 +0700

    feat(model): buat model WeatherLog untuk menyimpan data cuaca

---
+++ models/weatherLog.js
@@ -0,0 +1,22 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class WeatherLog extends Model {
+    static associate(models) {}
+  }
+  WeatherLog.init({
+    city: { type: DataTypes.STRING, allowNull: false },
+    country: { type: DataTypes.STRING, allowNull: false },
+    temperature: { type: DataTypes.DECIMAL(4, 1), allowNull: false },
+    humidity: { type: DataTypes.INTEGER, allowNull: false },
+    windSpeed: { type: DataTypes.DECIMAL(4, 1), allowNull: false },
+    weatherCondition: { type: DataTypes.STRING, allowNull: false },
+    airQualityIndex: { type: DataTypes.INTEGER, allowNull: false }
+  }, {
+    sequelize,
+    modelName: 'WeatherLog',
+    tableName: 'weather_logs'
+  });
+  return WeatherLog;
+};
